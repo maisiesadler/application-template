05884a1d7ef42b7cefb9848266f972f193b65695
Maisie <maisiesadler@hotmail.com>
2021-07-17 18:36:47 +0100

    Add lively

---
+++ tools/Application.Lively/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Lively;
+using Lively.Diagrams;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Lively
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembliesPath = "/Users/maisiesadler/repos/application-template/src/Application.Api/bin/Release/net6.0";
+            var assemblies = DependencyTreeConfigExtensions.GetAllAssembliesInDirectory(assembliesPath).ToList();
+
+            var config = new DependencyTreeConfig(assemblies, AssemblyConfiguration())
+            {
+                SkipTypes = new HashSet<string>
+                {
+                    "Microsoft.Extensions.Logging.ILogger`1",
+                },
+            };
+
+            var type = typeof(Application.Api.Controllers.ValidationController);
+            var tree = new DependencyTree(config);
+            var nodes = new List<DependencyTreeNode>
+            {
+                tree.GetDependencies(type.FullName),
+                tree.GetDependencies(typeof(Application.Domain.ValidationInteractor).FullName),
+            };
+
+            var diagram = PlantUml.Create(nodes);
+            System.Console.WriteLine(diagram);
+        }
+
+        private static IConfiguration AssemblyConfiguration()
+        {
+            var assemblyConfigLocation = "/Users/maisiesadler/repos/application-template/src/Application.Api/appsettings.json";
+            var cfgBuilder = new ConfigurationBuilder();
+            cfgBuilder.AddJsonFile(assemblyConfigLocation, optional: false, reloadOnChange: false);
+            return cfgBuilder.Build();
+        }
+    }
+}
---
+++ tools/Application.Lively/README.md
@@ -0,0 +1,3 @@
+# Example markdown file
+
+![plantuml.puml](http://www.plantuml.com/plantuml/proxy?cache=no&src=https://raw.githubusercontent.com/maisiesadler/application-template/tools/Application.Lively/classdiagram.puml)
